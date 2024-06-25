4ff2260d4b030b4c29b3ae20f80a2d55e3d179ec
Damoz1606 <panchitodmun@gmail.com>
2024-06-24 21:37:05 -0500

    :tada: Installation of tabler icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { useFetch } from "@/hooks/useFetch";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const {
+    data,
+    error,
+    loading,
+
+  } = useFetch('/api/news/first', 'GET');
+
+
   return (
     <main className={styles.main}>
-
+      
     </main>
   );
 }
